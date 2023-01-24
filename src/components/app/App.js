52e29f3b81e4bf049c12a8c0443752ad38556863
NyokabiKamau <nyokabikamau2014@gmail.com>
2023-01-24 17:58:55 +0300

    passed AnimalCard component to App.js

---
+++ src/components/app/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AnimalData from '../animal-items/AnimalData';
+// import './App.css';
+
+function App() {
+
+  return (
+    <div>
+      <AnimalData />
+    </div>
+  );
+}
+
+export default App;
