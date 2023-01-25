8c9c78647f302790f2ab4461e578b8936028efda
NyokabiKamau <nyokabikamau2014@gmail.com>
2023-01-25 22:17:15 +0300

    working on organization details

---
+++ src/components/organization-items/AnimalOrganizationDetails.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function AnimalOrganizationDetails() {
+    return(
+        <div>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default AnimalOrganizationDetails
