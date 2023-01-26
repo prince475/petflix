5ab778ed96f99c6bb7e46acbf40e19277ae14cf1
NyokabiKamau <nyokabikamau2014@gmail.com>
2023-01-26 20:03:16 +0300

    added an animal list card div

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import AnimalData from './components/animal-items/AnimalData';
 // import Test from './components/Test';
+// import AnimalList from './components/animal-items/AnimalList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <AnimalData />
+      {/* <AnimalList /> */}
     </div>
   );
 }
--- src/components/animal-items/AnimalList.js
+++ src/components/animal-items/AnimalList.js
@@ -1,11 +1,87 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-function AnimalList({animals}) {
+function AnimalList() {
+
+  const [animals, setAnimals] = useState([])
+
+  useEffect(() => {
+
+  fetch ("https://api.npoint.io/ad8fa9154a4336a9f6ff")
+  .then((r) => r.json())
+  .then(data => {
+    setAnimals(data)
+    console.log(data)
+  })
+  }, [])
+
+  const details = animals.map((animal) => {
     return (
-        <ul>
-            <div>{animals.type}</div>
-        </ul>
+      <div key={animal.id} className="col-3 p-1">
+        <div className="card h-100">
+            <img className="card-img-top" src={animal.photos.map((pic) => pic.small)} alt="{animal image}"/>
+            <div>{animal.name}</div>
+            <>{animal.age} . {animal.breeds.primary}</>
+        </div>
+  </div>
     )
-}
+  })
 
-export default AnimalList
+
+      return (
+        <div>
+          {details}
+        </div>
+        
+      );
+    }
+export default AnimalList;
+
+
+
+// import React, {useState, useEffect} from "react";
+
+// function AnimalList() {
+
+//     const [animals, setAnimals] = useState([])
+
+//     useEffect(() => {
+  
+//     fetch ("GET https://api.petfinder.com/v2/animals", 
+//     {
+//       method: "GET",
+//       headers: {
+//         "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJZWjNKc2VZOExDMDZPclIxOU9aSXVHOGJyc3lBZmtDblBYN2duVUhQUjlhdzN0ZjRPVyIsImp0aSI6ImI4MWI5MjAwNjBhYmM5MmRlZWNiOGIwZmE4MTkwNjUyMDM4MTdkNmU5ZTE0MzEwYTE5MmFkMTQ3YTVmNTdjNTE2OTgwMjY4ZWY3MWJhMDQ5IiwiaWF0IjoxNjc0NjcwMDgzLCJuYmYiOjE2NzQ2NzAwODMsImV4cCI6MTY3NDY3MzY4Mywic3ViIjoiIiwic2NvcGVzIjpbXX0.dYsYnN3sVhHZes1paMKG7x38O8kS4TIVIQ04RnaeJqKdHfRjACCOKBYy7_Qz2B8if99JZZc9iSiCJ2WhcFloFBteXql-KcQkOfPKbPk9PZJ9HUYQ0HFDExcma740npNiYlCr5lmWgE9dz6j7zARfZguhhVCm5GNFc1YGvGtJmgZzxfyuKlUEZR4UmVeOvxLvaFVE95X3fdxXTE5QiZkTXTfV4zekWQmEp_Fmgo3-t4yVu4hbVjrVYTmWBuEOEEHFCj0B3vx75q_ji5v7c873G6JLuV1RgyjbMm6VeT-05bHZjPX61FwPYnv0H08cbEGgxJgZOMT4xCIonJOQfvPutQ"
+//       }
+//     })
+  
+//       .then((response) => response.json())
+//       .then((data) => { 
+        
+//         setAnimals(data.animals)
+//         console.log(data.animals);
+        
+//       })
+//       .catch((error) => {
+//         console.error(error);
+//       });
+//     }, [])
+
+//     const list = animals.map((animal) => {
+//       return (
+  
+//         <div key={animal.id}>
+//             <ul>
+//                 <div>{animal.name}</div>
+//                  <div>
+//             </ul>
+//         </div>
+//       )
+//     })
+    
+//       return (
+//         <div>
+//           {list}
+//         </div>
+//       );
+//     }
+// export default AnimalList;
