b1b20867357b40e5d4bb2ad90bf2db995d8d4ef3
NyokabiKamau <nyokabikamau2014@gmail.com>
2023-01-28 17:43:28 +0300

    animal details is showing

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,12 +4,12 @@ import { Route, Switch} from "react-router-dom"
 import Login from '../login/Login';
 import Home from '../home/Home';
 import NavBar from '../navBar/NavBar';
-// import AnimalData from '../animal-items/AnimalCard/AnimalData';
+import AnimalData from '../animal-items/AnimalCard/AnimalData';
 import AnimalList from '../animal-items/AnimalList/AnimalList';
-import AnimalCard from '../animal-items/AnimalCard/AnimalCard';
-// import AnimalOrganizationList from '../organization-items/AnimalOrganizationList'
 
-// import AnimalOrganizationData from '../organization-items/AnimalOrganizationData'
+//import AnimalOrganizationList from '../organization-items/AnimalOrganizationList'
+
+import AnimalOrganizationData from '../organization-items/AnimalOrganizationData'
 // import Breeds from '../breeds/Breeds';
 
 
@@ -27,8 +27,8 @@ function App() {
      <div className='wrapper'>
       <Switch>
           <Route exact path="/animals" > <AnimalList/> </Route>
-          <Route exact path="/card"><AnimalCard/></Route>
-          {/* <Route exact path="/organization" > <AnimalOrganizationData/> </Route> */}
+          <Route exact path="/card"><AnimalData/></Route>
+          <Route exact path="/organization" > <AnimalOrganizationData/> </Route>
           {/* <Route exact path="/type" > <Breeds/> </Route> */}
           {/* <Route exact path="/organization"><AnimalCard /></Route> */}
           <Route exact path="/"> <Home /> </Route>
--- src/components/preference/Prefrence.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function Preference() {
-    return (
-        <h2>Preference</h2>
-    )
-}
